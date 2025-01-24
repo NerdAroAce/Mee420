5382b217ad8c9765b103a449bfaf4e689528f90b
NerdAroAce <sveps29@gmail.com>
2025-01-25 00:39:51 +0200

    forgor a :

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ class MyClient(discord.Client):
     def __init__(self, *, intents: discord.Intents):
         super().__init__(intents=intents)
         self.tree = app_commands.CommandTree(self)
-    async def setup_hook(self)
+    async def setup_hook(self):
         await self.tree.sync()
     async def on_message(self, message):
         if message.content == "hewwo":
