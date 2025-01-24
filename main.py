30f2b7392997f72476010f0f73bc7d24f6751f86
NerdAroAce <sveps29@gmail.com>
2025-01-25 00:30:22 +0200

    created main.py and wrote basic stuff
    uwu

---
+++ main.py
@@ -0,0 +1,23 @@
+import requests
+import time
+import json
+import random
+import discord
+from discord.ext import commands
+from discord import app_commands
+from discord.ext import tasks
+
+f = open("token.sensitive")
+TOKEN = f.readline()
+f.close()
+
+class MyClient(discord.Client):
+    """Copy pasted from random github repo"""
+    def __init__(self, *, intents: discord.Intents):
+        super().__init__(intents=intents)
+        self.tree = app_commands.CommandTree(self)
+    async def setup_hook(self)
+        await self.tree.sync()
+intents = discord.Intents.all()
+client = MyClient(intents=intents)
+client.run(TOKEN)
