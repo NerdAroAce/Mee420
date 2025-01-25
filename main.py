991df2720e3d9a0b6ba92de959be563eae50af3e
TJC472 <124094785+TJC47@users.noreply.github.com>
2025-01-25 01:23:06 +0100

    fix bot also better formating

--- main.py
+++ main.py
@@ -22,11 +22,14 @@ class MyClient(discord.Client):
            await message.channel.send("I'm better than that bastard.")
         if message.content == "m!furry":
             await message.channel.send(f"You are {str(random.randint(0,100))}% a furry.")
+
+intents = discord.Intents.all()
+client = MyClient(intents=intents)
+
 @client.tree.command(description="Tells you how much of a furry you are :3")
 @app_commands.allowed_installs(guilds=True, users=True)
 @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
 async def furry(interaction: discord.Interaction,):
-   await interaction.response.send_message(content=f"You are {str(random.randint(0,100))}% a furry.")      
-intents = discord.Intents.all()
-client = MyClient(intents=intents)
+   await interaction.response.send_message(content=f"You are {str(random.randint(0,100))}% a furry.")   
+
 client.run(TOKEN)
