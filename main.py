f8b820012d672854b691d9a855b7ab511c93d531
NerdAroAce <sveps29@gmail.com>
2025-01-25 06:02:58 +0200

    Forgor a ":"

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ async def ban(interaction: discord.Interaction, member: discord.Member, reason: 
 
 @client.tree.command(description="Unbans a user")
 @commands.has_permissions(administrator=True)
-async def uban(interaction: discord.Interaction, member: discord.Member, reason: str = None)
+async def uban(interaction: discord.Interaction, member: discord.Member, reason: str = None):
     await member.unban
     if reason:
         await interaction.response.send_message(content=f"{member} was unbanned. Admin note: {reason}")
